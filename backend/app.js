d427dd2c782e3c681aa3b8158c5110ffc6a0d2a5
lasertruf <71066096+lasertruf@users.noreply.github.com>
2021-07-21 20:36:29 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use((req,res,next)=>{
     res.header("Access-Control-Allow-Origin","*");
-    res.hader('Access-Control-Allow-Methods','GET,POST,OPTIONS, DELETE,PUT');
+    res.header('Access-Control-Allow-Methods','GET,POST,OPTIONS, DELETE,PUT');
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Accept");
     next();
      
