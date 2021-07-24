a3b056ad57d4628e917025c6a263e439fd13c5df
lasertruf <jayantwaghmare9@gmail.com>
2021-07-24 16:18:39 +0530

    ok

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/socialDB', (err)=> {
+const dburl = "mongodb://socialapp-dbcosmos:0HrV33P50BhpfkWXV1bP51Rjk8FZucxD56ne6y0pqtSefxBpv3PdYRlJGpSYdCwElXicxARNuLm0wvSVQx1EPQ%3D%3D@socialapp-dbcosmos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@socialapp-dbcosmos@";
+mongoose.connect(dburl, (err)=> {
     if(!err){
         console.log("Successful Connection");
     }
